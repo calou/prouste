b31b01b87e033635e5e1d3f5b3d90daad2f758cb
Sébastien Gruchet <sebastien.gruchet@digimind.com>
2019-11-11 10:36:09 +0100

    Add title extractor

--- src/extractor.rs
+++ src/extractor.rs
@@ -1,12 +1,44 @@
 extern crate select;
 
 use std::string::String;
-//use select::predicate::{Attr, Name};
+use select::predicate::{Attr, Name};
 
 pub mod extractor {
     use select::document::Document;
+    use super::select::predicate::Name;
+
+    struct TextExtraction {
+        successful: bool,
+        text: String,
+    }
+
+    trait  TextExtractor {
+        fn extract(&self, document: Document) -> TextExtraction;
+    }
+
+    struct TitleTagExtractor;
+
+    impl TextExtractor for TitleTagExtractor {
+        fn extract(&self, document: Document) -> TextExtraction {
+            let text = match document.find(Name("title")).next() {
+                Some(node) => node.text(),
+                _ => String::new()
+            };
+            return TextExtraction {
+                successful: text != "",
+                text
+            }
+        }
+    }
 
     pub fn get_title(document: Document) -> String {
+        let text_extractors = [ TitleTagExtractor ];
+        for text_extractor in &text_extractors {
+            let text_extraction = text_extractor.extract(document.to_owned());
+            if text_extraction.successful {
+                return text_extraction.text;
+            }
+        }
         return String::new();
     }
 
