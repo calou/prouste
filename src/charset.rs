aaa10759bc31c5ea3a57e103a78b33bdfbc8598d
Sébastien Gruchet <sebastien.gruchet@digimind.com>
2019-11-11 07:29:49 +0100

    Add get_charset

--- src/charset.rs
+++ src/charset.rs
@@ -1,60 +1,60 @@
 
+pub mod charset {
+    pub fn normalize(character_set: &str) -> String {
+        let cs = character_set.to_ascii_uppercase();
+        return match cs.as_str() {
+            "UTF8" | "UT-8" | "UTR-8" | "UFT-8" | "UTF8-WITHOUT-BOM" | "UTF8_GENERAL_CI" => String::from("UTF-8"),
 
-pub fn normalise_charset(character_set: &str) -> String {
-    let cs = character_set.to_ascii_uppercase();
-    println!("{}", cs.as_str());
-    return match cs.as_str() {
-        "UTF8" | "UT-8" | "UTR-8" | "UFT-8" | "UTF8-WITHOUT-BOM" | "UTF8_GENERAL_CI" => String::from("UTF-8"),
+            // override Japanese
+            // CP943: IBM OS/2 Japanese, superset of Cp932 and Shift-JIS
+            "CP943" | "CP943C" | "SIFT_JIS" | "SHIFT-JIS" => String::from("SHIFT_JIS"),
 
-        // override Japanese
-        // CP943: IBM OS/2 Japanese, superset of Cp932 and Shift-JIS
-        "CP943" | "CP943C" | "SIFT_JIS" | "SHIFT-JIS" => String::from("SHIFT_JIS"),
+            // override Korean
+            "EUC-KR" | "MS949" | "KSC5601" | "WINDOWS-949" | "KS_C_5601-1987" | "KSC_5601" => String::from("UHC"),
 
-        // override Korean
-        "EUC-KR" | "MS949" | "KSC5601" | "WINDOWS-949" | "KS_C_5601-1987" | "KSC_5601" => String::from("UHC"),
+            // override Thai
+            //case "TIS-620", "WINDOWS-874":
+            //	return "ISO-8859-11"
 
-        // override Thai
-        //case "TIS-620", "WINDOWS-874":
-        //	return "ISO-8859-11"
+            // override latin-2
+            "LATIN2_HUNGARIAN_CI" | "LATIN2" => String::from("LATIN-2"),
 
-        // override latin-2
-        "LATIN2_HUNGARIAN_CI" | "LATIN2" => String::from("LATIN-2"),
+            // override cyrillic
+            "WIN1251" | "WIN-1251" | "WINDOWS-1251" => String::from("CP1251"),
 
-        // override cyrillic
-        "WIN1251" | "WIN-1251" | "WINDOWS-1251" => String::from("CP1251"),
+            // override Hebrew
+            "WINDOWS-1255" => String::from("ISO-8859-8"),
 
-        // override Hebrew
-        "WINDOWS-1255" => String::from("ISO-8859-8"),
+            // override Turkish
+            //case "WINDOWS-1254":
+            //	return "ISO-8859-9"
+            // override the parsing of ISO-8859-1 to behave as Windows-1252 (CP1252):
+            // in ISO-8859-1, everything from 128-255 in the ASCII table are ctrl characters,
+            // whilst in CP1252 they're symbols
 
-        // override Turkish
-        //case "WINDOWS-1254":
-        //	return "ISO-8859-9"
-        // override the parsing of ISO-8859-1 to behave as Windows-1252 (CP1252):
-        // in ISO-8859-1, everything from 128-255 in the ASCII table are ctrl characters,
-        // whilst in CP1252 they're symbols
+            // override Baltic
+            "WINDOWS-1257" => String::from("ISO-8859-13"),
 
-        // override Baltic
-        "WINDOWS-1257" => String::from("ISO-8859-13"),
+            "ANSI" | "LATIN-1" | "ISO" | "RFC" | "MACINTOSH" | "8859-1" | "8859-15" | "ISO8859-1" | "ISO8859-15" | "ISO-8559-1" | "ISO-8859-1" | "ISO-8859-15" => String::from("CP1252"),
 
-        "ANSI" | "LATIN-1" | "ISO" | "RFC" | "MACINTOSH" | "8859-1" | "8859-15" | "ISO8859-1" | "ISO8859-15" | "ISO-8559-1" | "ISO-8859-1" | "ISO-8859-15" => String::from("CP1252"),
+            _ => String::from(character_set).to_ascii_uppercase()
+        };
+    }
 
-        _ => String::from(character_set).to_ascii_uppercase()
-    };
-}
+    #[cfg(test)]
+    mod tests {
+        use std::string::String;
 
-#[cfg(test)]
-mod tests {
-    use std::string::String;
+        // Note this useful idiom: importing names from outer (for mod tests) scope.
+        use super::*;
 
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn test_normalise_charset() {
-        assert_eq!(normalise_charset("dummy"), "DUMMY");
-        assert_eq!(normalise_charset("UTF8"), "UTF-8");
-        assert_eq!(normalise_charset("utf-8"), "UTF-8");
-        assert_eq!(normalise_charset("MACINTOSH"), "CP1252");
-        assert_eq!(normalise_charset("WINDOWS-1255"), "ISO-8859-8");
+        #[test]
+        fn test_normalize() {
+            assert_eq!(normalize("dummy"), "DUMMY");
+            assert_eq!(normalize("UTF8"), "UTF-8");
+            assert_eq!(normalize("utf-8"), "UTF-8");
+            assert_eq!(normalize("MACINTOSH"), "CP1252");
+            assert_eq!(normalize("WINDOWS-1255"), "ISO-8859-8");
+        }
     }
 }
--- src/crawler.rs
+++ src/crawler.rs
@@ -4,9 +4,11 @@ use std::string::String;
 
 use select::document::Document;
 use select::predicate::{Attr, Name};
+
+use crate::charset::charset;
+
 use self::select::predicate::Predicate;
 
-use crate::charset::normalise_charset;
 pub fn add_spaces_between_tags(text: String) -> String {
     return text.replace("<img ", "\n<img ")
         .replace("</blockquote>", "</blockquote>\n")
@@ -31,16 +33,30 @@ pub fn get_charset_from_content_type(content_type: String) -> String {
     let cs = content_type.to_ascii_lowercase();
     let idx = cs.find("charset=");
     match idx {
-        Some(i) => {return normalise_charset(cs.get(i+"charset=".len()..).unwrap());}
-        _ => {return String::new();}
+        Some(i) => { return charset::normalize(cs.get(i + "charset=".len()..).unwrap()); }
+        _ => { return String::new(); }
     }
-    /*cs = strings.TrimPrefix(cs, "text/html;charset=")
-    cs = strings.TrimPrefix(cs, "text/xhtml;charset=")
-    cs = strings.TrimPrefix(cs, "application/xhtml+xml;charset=")
-    */
-//return NormaliseCharset(cs)
     return cs;
 }
+
+// GetCharset returns a normalised charset string extracted from the meta tags
+pub fn get_charset(document: Document) -> String {
+    let clone = document.clone();
+    let ct = get_content_type(document);
+
+    if "" != ct && ct.to_ascii_lowercase().contains("charset") {
+        return get_charset_from_content_type(ct);
+    }
+
+    // <meta charset="utf-8">
+    for node in clone.find(Name("meta")) {
+        if node.attr("charset").is_some() {
+            return charset::normalize(node.attr("charset").unwrap());
+        }
+    }
+    return String::new();
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -70,7 +86,12 @@ mod tests {
 
     #[test]
     fn test_get_charset_from_content_type() {
-        let document = Document::from("<html><head></head></html>");
         assert_eq!(get_charset_from_content_type(String::from("text/html; charset=utf-8")), "UTF-8");
+    }
+
+    #[test]
+    fn test_get_charset() {
+        assert_eq!(get_charset(Document::from("<html><head><meta charset=\"UTF8\"></head></html>")), "UTF-8");
+        assert_eq!(get_charset(Document::from("<html><head><meta test=\"\"><meta charset=\"dummy\"></head></html>")), "DUMMY");
     }
 }
