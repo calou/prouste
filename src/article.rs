88a5415ef37de45507c23cf9275f92deea6988c4
Sébastien Gruchet <sebastien.gruchet@digimind.com>
2019-11-16 08:40:55 +0100

    Add crate on article fields

--- src/article.rs
+++ src/article.rs
@@ -1,27 +1,17 @@
-
-#[derive(Debug, Clone, Default)]
+#[derive(PartialEq, Debug, Clone, Default)]
 pub struct Article {
-    pub title: String,
-    pub text: String,
-    pub language: String,
-    pub favico: String,
-    pub canonical_link: String,
-    pub meta_keywords: String,
-    pub top_image: String,
-    pub links: Vec<String>,
+    pub(crate) title: String,
+    pub(crate) text: String,
+    pub(crate) language: String,
+    pub(crate) favico: String,
+    pub(crate) canonical_link: String,
+    pub(crate) meta_keywords: String,
+    pub(crate) top_image: String,
+    pub(crate) links: Vec<String>,
 }
 
 impl Article {
     pub fn new() -> Self {
         Self::default()
-    }
-    pub fn title(self: Self) -> String {
-        self.title
-    }
-    pub fn language(self: Self) -> String {
-        self.language
-    }
-    pub fn text(self: Self) -> String {
-        self.text
     }
 }
